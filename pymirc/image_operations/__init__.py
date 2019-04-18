e8a790be17d97881db4b75bfab9bf708c94ca724
Georg Schramm <georg.schramm@kuleuven.be>
2019-04-18 15:04:12 +0200

    add routine + example on how to convert a 2D binary image (segmentation) into a set of contours

--- pymirc/image_operations/__init__.py
+++ pymirc/image_operations/__init__.py
@@ -1,5 +1,6 @@
-from .aff_transform            import aff_transform, kul_aff
-from .backward_3d_warp         import backward_3d_warp
-from .random_deformation_field import random_deformation_field
-from .zoom3d                   import zoom3d, downsample_3d_0, downsample_3d_1, downsample_3d_2
-from .zoom3d                   import upsample_3d_0, upsample_3d_1, upsample_3d_2
+from .aff_transform               import aff_transform, kul_aff
+from .backward_3d_warp            import backward_3d_warp
+from .random_deformation_field    import random_deformation_field
+from .zoom3d                      import zoom3d, downsample_3d_0, downsample_3d_1, downsample_3d_2
+from .zoom3d                      import upsample_3d_0, upsample_3d_1, upsample_3d_2
+from .binary_2d_image_to_contours import binary_2d_image_to_contours
---
+++ pymirc/image_operations/binary_2d_image_to_contours.py
@@ -0,0 +1,92 @@
+import numpy as np
+
+from copy import deepcopy
+from matplotlib.patches import Polygon 
+
+from skimage import measure
+from scipy.ndimage.morphology import binary_fill_holes
+
+def binary_2d_image_to_contours(img, include_holes = True):
+  """Conversion of a 2d binary image (segmentation) into a set of contours
+
+  Parameters
+  ----------
+  img : 2d binary numpy array
+    containing the pixelized segmentation
+
+  include_holes: bool, optional
+    whether to computer contours of holes insides objects and to include
+    them as well (default True)
+
+  Returns
+  -------
+  list
+    of Nx2 numpy arrays containg the contours
+
+  Notes
+  -----
+  Finding the contours is performed via the marching squares algorithm from skimage.
+  First, the contours of the image with filled holes are generated. 
+  Second (optional), the contours of the holes are generated and connected to
+  their parent contours.
+  """
+  # we have to 0 pad the image, otherwise the contours are not closed
+  # at the outside
+  bin_img = np.pad(img, 1, 'constant')
+  
+  bin_img_filled = binary_fill_holes(bin_img)
+  
+  bin_holes = bin_img_filled - bin_img
+  
+  # get the outer contours of the filled image (no holes)
+  outer_contours = measure.find_contours(bin_img_filled == 1, 0.5, positive_orientation = 'high')
+  # get the inner contours (contours around holes)
+  inner_contours = measure.find_contours(bin_holes == 1, 0.5, positive_orientation = 'low')
+  
+  contours = deepcopy(outer_contours)
+  
+  # test in which outer contour a given inner contour lies
+  outer_polys = []
+  for i in range(len(outer_contours)):
+    outer_polys.append(Polygon(outer_contours[i], True))
+  
+  parent_contour_number = np.zeros(len(inner_contours), dtype = int)
+  for j in range(len(inner_contours)):
+    for i in range(len(outer_contours)):
+      if outer_polys[i].contains_point(inner_contours[j][0,:]):
+        parent_contour_number[j] = i
+  
+  # get the closest point on the outer contours for the start point
+  # of the inner contour
+  
+  if include_holes:
+    closest_outer_points = []
+    closest_outer_points_is_in_contour = []
+    for j in range(len(inner_contours)):
+      oc  = deepcopy(outer_contours[parent_contour_number[j]])
+   
+      ip    = inner_contours[j][0,:]
+      dist  = np.apply_along_axis(lambda x: (x[0] - ip[0])**2 + (x[1] - ip[1])**2, 1, oc)
+      closest_outer_points.append(oc[np.argmin(dist),:])
+   
+      cop = deepcopy(closest_outer_points[j])
+    
+      # check if closet point is already in contour
+      closest_outer_points_is_in_contour.append(np.any(np.all(np.isin(oc,cop,True),axis=1)))
+    
+      con = deepcopy(contours[parent_contour_number[j]])
+    
+      i_point = np.where(np.apply_along_axis(lambda x: np.array_equal(x,cop),1,con))[0][0]
+     
+      contours[parent_contour_number[j]] = np.concatenate([con[:(i_point+1),:],
+                                                           inner_contours[j],
+                                                           np.array(cop).reshape(1,2),  
+                                                           con[i_point:,:]])
+  
+  # subtract 1 from the contour coordinates because we had to use 0 padding
+  for i in range(len(contours)):
+     contours[i] -= 1
+
+  return contours
+
+
