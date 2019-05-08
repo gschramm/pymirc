56008039a046941e8dbebceac46eaab1185520b9
Georg Schramm <georg.schramm@kuleuven.be>
2019-05-08 15:35:36 +0200

    bugfix: remove squared_euclidean_dist from __init__

--- pymirc/registration/__init__.py
+++ pymirc/registration/__init__.py
@@ -1,1 +1,1 @@
-from .cost_functions import jointhisto_3dvols, neg_mutual_information, squared_euclidean_dist, regis_cost_func
+from .cost_functions import jointhisto_3dvols, neg_mutual_information, regis_cost_func
