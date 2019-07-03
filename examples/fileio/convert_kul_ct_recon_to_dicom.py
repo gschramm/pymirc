a013fafedf3c6f6c1c444c7145af0bb045701697
Georg Schramm <georg.schramm@kuleuven.be>
2019-07-03 12:34:06 +0200

    change default name of output dicom directory

--- examples/fileio/convert_kul_ct_recon_to_dicom.py
+++ examples/fileio/convert_kul_ct_recon_to_dicom.py
@@ -9,10 +9,11 @@ from scipy.io    import readsav
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument('kul_sav_file', help = 'filename of IDL sav file with KUL recon')
-parser.add_argument('ref_dcm_dir',  help = 'directory containing the reference dicom files')
-parser.add_argument('output_dir',   help = 'directory containing the reference dicom files')
+parser.add_argument('kul_sav_file',   help = 'filename of IDL sav file with KUL recon')
+parser.add_argument('ref_dcm_dir',    help = 'directory containing the reference dicom files')
 
+parser.add_argument('--output_dir',         help = 'name of the output directory', default = '.')
+parser.add_argument('--output_suffix',      help = 'suffix for the output directory', default = None)
 parser.add_argument('--ref_dcm_pat',        help = 'file pattern for reference dicom files', default = '*')
 parser.add_argument('--dcm_tag_file',       help = 'txt file with dcm tags to copy', 
                     default = 'ct_dcm_tags_to_copy.txt')
@@ -21,16 +22,8 @@ parser.add_argument('--series_desc_prefix', help = 'prefix for dcm series descri
                     default = '(UZL motion corrected)')
 
 args       = parser.parse_args()
-output_dir = args.output_dir
 
 #-----------------------------------------------------------------------------------------
-
-if os.path.isdir(output_dir):
-  raise FileExistsError('output directory ' + output_dir + ' already exists')
-
-# load the list of dicom tags to copy from the reference header from an input text file
-with open(args.dcm_tag_file,'r') as f:
-  tags_to_copy = [x.strip() for x in f.read().splitlines()]
 
 # read the reference dicom volume
 ref_dcm = pymf.DicomVolume(os.path.join(args.ref_dcm_dir, args.ref_dcm_pat))
@@ -45,11 +38,27 @@ kul_recon = np.swapaxes(kul_recon, 0, 2)
 # to get the KUL recon in LPS we have to reverse the last axix
 kul_recon = np.flip(kul_recon,2)
 
+# load the list of dicom tags to copy from the reference header from an input text file
+with open(args.dcm_tag_file,'r') as f:
+  tags_to_copy = [x.strip() for x in f.read().splitlines()]
+
 # create the dictionary of tags and values that are copied from the reference dicom header
 dcm_header_kwargs = {}
 for tag in tags_to_copy:
   if tag in ref_dcm.firstdcmheader:
     dcm_header_kwargs[tag] = ref_dcm.firstdcmheader.data_element(tag).value
+
+# create the name of the output directoty
+if args.output_suffix is None:
+  output_suffix = os.path.splitext(os.path.basename(args.kul_sav_file))[0] + '_' + args.kul_var_name
+else:
+  output_suffix = output_suffix
+
+output_dir = os.path.join(args.output_dir, ref_dcm.firstdcmheader.StudyInstanceUID + '_' + ref_dcm.firstdcmheader.AccessionNumber + '_' + output_suffix)
+
+if os.path.isdir(output_dir):
+  raise FileExistsError('output directory ' + output_dir + ' already exists')
+
 
 # write the dicoms
 # the number of tags to be copied from the original recon can be extented
@@ -62,20 +71,21 @@ dcm_out_fnames = pymf.write_3d_static_dicom(kul_recon, output_dir,
 
 # for PACS import we have to write two additional text files
 dcm_props = {}
-dcm_props['import.type']       = 'cd'
-dcm_props['study.size']        = kul_recon.shape[2] 
-dcm_props['version']           = 2
-dcm_props['study.uid']         = ref_dcm.firstdcmheader.StudyInstanceUID
-dcm_props['institution']       = 'UZL'
-dcm_props['importer.computer'] = os.uname().nodename
-dcm_props['study.date']        = ref_dcm.firstdcmheader.StudyDate[:8]
-dcm_props['importer.login']    = 'Johan Nuyts'
-dcm_props['patient.ead']       = ref_dcm.firstdcmheader.PatientID
-dcm_props['file.list']         = 'filelist.txt'
-dcm_props['werkomgeving']      = 'UZL'
-dcm_props['study.time']        = ref_dcm.firstdcmheader.StudyTime[:6]
-dcm_props['modality']          = ref_dcm.firstdcmheader.Modality
-dcm_props['study.description'] = ref_dcm.firstdcmheader.StudyDescription
+dcm_props['import.type']            = 'reconstruction'
+dcm_props['study.size']             = kul_recon.shape[2] 
+dcm_props['version']                = 2
+dcm_props['study.uid']              = ref_dcm.firstdcmheader.StudyInstanceUID
+dcm_props['institution']            = 'UZL'
+dcm_props['importer.computer']      = os.uname().nodename
+dcm_props['study.date']             = ref_dcm.firstdcmheader.StudyDate[:8]
+dcm_props['importer.login']         = 'Johan Nuyts'
+dcm_props['patient.ead']            = ref_dcm.firstdcmheader.PatientID
+dcm_props['file.list']              = 'filelist.txt'
+dcm_props['werkomgeving']           = 'UZL'
+dcm_props['study.time']             = ref_dcm.firstdcmheader.StudyTime[:6]
+dcm_props['modality']               = ref_dcm.firstdcmheader.Modality
+dcm_props['study.description']      = ref_dcm.firstdcmheader.StudyDescription
+dcm_props['study.accession.number'] = ref_dcm.firstdcmheader.AccessionNumber
 
 with open(os.path.join(output_dir, 'dicomimport.properties'), 'w') as f:
   for key, value in dcm_props.items():
