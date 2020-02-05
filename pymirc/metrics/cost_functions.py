86c8c4e5b7a6d0bbe0331e74202c53134f120651
Tom Eelbode <tom.eelbode@kuleuven.be>
2020-02-05 14:22:30 +0100

    Reorganisation of metrics and loss functions

--- pymirc/metrics/cost_functions.py
+++ pymirc/metrics/cost_functions.py
@@ -12,12 +12,12 @@ def jointhisto_3dvols(x, y, nbins = 40, normalize = True):
 
   Parameters
   ----------
-  x : 3D numpy array 
+  x : 3D numpy array
     first input
 
-  y : 3D numpy array 
+  y : 3D numpy array
     second input
-  
+
   nbins : int, optional
     number of bins in the joint histogram (default 40)
 
@@ -26,19 +26,19 @@ def jointhisto_3dvols(x, y, nbins = 40, normalize = True):
 
   Returns
   -------
-  2D numpy array of shape (nbins, nbins) 
+  2D numpy array of shape (nbins, nbins)
     containing the joint histogram
 
   Note
   ----
-  E.g. useful for rigid registration of two 3d volumes with 
+  E.g. useful for rigid registration of two 3d volumes with
   using mutual information.
-  
+
   The implementaion is optimized for a multi cpu system using
   numba.
 
   The left edges of the bins are at np.arange(nbins) * binwidth + jmin
-  where jmin is the joint minimum between x and y and 
+  where jmin is the joint minimum between x and y and
   binwidth = (jmax - jmin) / (nbins - 1).
   """
 
@@ -47,7 +47,7 @@ def jointhisto_3dvols(x, y, nbins = 40, normalize = True):
 
   ymin = y.min()
   ymax = y.max()
-  
+
   xbinwidth = (xmax - xmin) / (nbins - 1)
   ybinwidth = (ymax - ymin) / (nbins - 1)
 
@@ -74,51 +74,51 @@ def jointhisto_3dvols(x, y, nbins = 40, normalize = True):
 
   return jhisto
 
- 
-#---------------------------------------------------------------- 
+
+#----------------------------------------------------------------
 def neg_mutual_information(x, y, nbins = 40, norm = True):
   """Negative mutual information between two 3D volumes
 
   Parameters
   ----------
-  x : 3D numpy array 
+  x : 3D numpy array
     first input
 
-  y : 3D numpy array 
+  y : 3D numpy array
     second input
- 
+
   nbins : int, optional
     number of bins in the joint histogram (default 40)
-  
+
   norm: bool, optional
-    whether to use normalized version of MI (default True) 
+    whether to use normalized version of MI (default True)
 
   Returns
   -------
   float
     containing the negative mutual information
-  
+
   References
   ----------
   Maes et al: IEEE TRANSACTIONS ON MEDICAL IMAGING, VOL. 16, NO. 2, APRIL 1997
   Studholme et al: Pattern Recognition 32 (1999) 71 86
   """
   p_xy  = jointhisto_3dvols(x, y, nbins = nbins, normalize = True)
-  ixy   = np.where(p_xy > 0) 
+  ixy   = np.where(p_xy > 0)
 
   # calculate the outer product of the marginal distributions
   p_x     = p_xy.sum(axis = 1)
   p_y     = p_xy.sum(axis = 0)
 
   if norm:
-    # normalized mututal information 
+    # normalized mututal information
     # Studholme et al: Pattern Recognition 32 (1999) 71 86
     # has 1:1 corespondance to ECC defined by Maes et al
 
-    ix = np.where(p_x > 0)    
-    iy = np.where(p_y > 0)    
+    ix = np.where(p_x > 0)
+    iy = np.where(p_y > 0)
 
-    mi = -(((p_x[ix]*np.log(p_x[ix])).sum() +  (p_y[iy]*np.log(p_y[iy])).sum()) / 
+    mi = -(((p_x[ix]*np.log(p_x[ix])).sum() +  (p_y[iy]*np.log(p_y[iy])).sum()) /
            (p_xy[ixy]*np.log(p_xy[ixy])).sum())
 
   else:
@@ -126,10 +126,10 @@ def neg_mutual_information(x, y, nbins = 40, norm = True):
     # Maes et al: IEEE TRANSACTIONS ON MEDICAL IMAGING, VOL. 16, NO. 2, APRIL 1997
     p_x_p_y = np.outer(p_x, p_y)
     mi = -(p_xy[ixy] * np.log(p_xy[ixy]/p_x_p_y[ixy])).sum()
- 
+
   return mi
 
-#---------------------------------------------------------------- 
+#----------------------------------------------------------------
 def regis_cost_func(params, img_fix, img_float, verbose = False,
                     rotate = True, metric = neg_mutual_information, pre_affine = None, metric_kwargs = {}):
   """Generic cost function for rigid registration
@@ -140,7 +140,7 @@ def regis_cost_func(params, img_fix, img_float, verbose = False,
   params : 3 or 6 element numpy array
     If rotate is False it contains the 3 translations
     If rotate is True  it contains the 3 translations and the 3 rotation angles
- 
+
   img_fix : 3D numpy array
      containg the fixed (reference) volume
 
@@ -151,8 +151,8 @@ def regis_cost_func(params, img_fix, img_float, verbose = False,
     print verbose output (default False)
 
   rotate : bool, optional
-    rotate volume as well on top of translations (6 degrees of freedom)  
- 
+    rotate volume as well on top of translations (6 degrees of freedom)
+
   metric : function(img_fix, aff_transform(img_float, ...)) -> R, optional
     metric used to compare fixed and floating volume (default neg_mutual_information)
 
