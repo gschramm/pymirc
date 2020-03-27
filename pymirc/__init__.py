0f6f2e838a41b22140d8ae678afcfdc3c16a2917
Georg Schramm <georg.schramm@kuleuven.be>
2020-03-27 12:46:55 +0100

    revert changes in __init__.py to get version number at runtime

--- pymirc/__init__.py
+++ pymirc/__init__.py
@@ -2,3 +2,11 @@ from . import fileio
 from . import image_operations
 from . import metrics
 from . import viewer
+
+# this is needed to get the package version at runtime
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
