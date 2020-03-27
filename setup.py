895ff28fcebdff21257c8a5a96c1a2534bef50f8
Georg Schramm <georg.schramm@kuleuven.be>
2020-03-27 12:38:36 +0100

    BUGGIX: remove bugs in setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ import setuptools
 setuptools.setup(
     name="pymirc",
     use_scm_version={'fallback_version':'unkown'},
-    setup_requires=['setuptools_scm, setuptools_scm_git_archive'],
+    setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
     author="Georg Schramm, Tom Eelbode, Jeroen Bertels",
     author_email="georg.schramm@kuleuven.be",
     description="Python imaging utilities developed in the medical imaging research center of KU Leuven",
     license='LGPL v3',
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gschramm/pymirc",
     packages=setuptools.find_packages(),
