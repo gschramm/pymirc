136b82ab7cc49db4c64218cb166efd5431889d41
Georg Schramm <georg.schramm@kuleuven.be>
2020-07-01 11:27:19 +0200

    update example for displaying nrrd volumes and segmentation

--- examples/fileio/view_nrrd_seg.py
+++ examples/fileio/view_nrrd_seg.py
@@ -3,10 +3,14 @@
 
 # to run it you need to install pynrrd
 
+import argparse
 import nrrd
 import numpy as np
+import matplotlib.pyplot as py
 import pymirc.image_operations as pi
 import pymirc.viewer as pv
+
+#----------------------------------------------------------------------------
 
 def get_lps_affine_from_hdr(hdr):
   aff = np.eye(4)
@@ -23,14 +27,33 @@ def get_lps_affine_from_hdr(hdr):
 
   return aff
 
-ct, ct_hdr   = nrrd.read('CTChest.nrrd')
-seg, seg_hdr = nrrd.read('Segmentation preview.seg.nrrd')  
+#----------------------------------------------------------------------------
+
+# parse the command line
+parser = argparse.ArgumentParser()
+parser.add_argument('ct_file',  help = 'CT nrrd file')
+parser.add_argument('seg_file', help = 'segmentation nrrd file')
+args = parser.parse_args()
+
+ct, ct_hdr   = nrrd.read(args.ct_file)
+seg, seg_hdr = nrrd.read(args.seg_file)
 
 ct_aff  = get_lps_affine_from_hdr(ct_hdr)
 seg_aff = get_lps_affine_from_hdr(seg_hdr) 
 
 ct_voxsize = np.sqrt((ct_aff**2).sum(0))[:-1]
 
-seg2 = pi.aff_transform(seg, np.linalg.inv(seg_aff) @ ct_aff, ct.shape, trilin = False)
+# the segmentation array usually a crop of the original array
+seg_offset = np.round((seg_aff[:-1,-1] - ct_aff[:-1,-1]) / np.diag(ct_aff)[:3]).astype(int)
+seg2 = np.zeros(ct.shape, dtype = np.int8)
+seg2[seg_offset[0]:(seg_offset[0]+seg.shape[0]), seg_offset[1]:(seg_offset[1]+seg.shape[1]),
+     seg_offset[2]:(seg_offset[2]+seg.shape[2])] = seg
+seg2_aff = ct_aff.copy()
 
-pv.ThreeAxisViewer([ct,seg2], voxsize=ct_voxsize)
+# reorient the images to standard LPS orientation
+ct, ct_aff = pi.reorient_image_and_affine(ct, ct_aff)
+seg2, seg2_aff = pi.reorient_image_and_affine(seg2, seg2_aff)
+
+imshow_kwargs = [{'cmap':py.cm.Greys_r, 'vmin':-300, 'vmax':300}, {}]
+
+pv.ThreeAxisViewer([ct,seg2], voxsize = ct_voxsize, imshow_kwargs = imshow_kwargs)
