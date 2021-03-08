81c87c3bc0599209ccc971d7f556ac76b088c369
Georg Schramm <georg.schramm@posteo.net>
2021-03-08 16:04:11 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setuptools.setup(
     license='LGPL v3',
     long_description_content_type="text/markdown",
     url="https://github.com/gschramm/pymirc",
-    packages=setuptools.find_packages(exclude = ["data","examples","tmp","wip"]),
+    packages=setuptools.find_packages(exclude = ["data","examples","tutorial"]),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: LGPL License",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6, <3.9',
