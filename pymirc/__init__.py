c69b7845f821931daaba0feb3c8801375d085e35
Georg Schramm <georg@Georgs-MacBook-Pro-5.local>
2024-11-27 23:36:22 +0100

    add __version__

--- pymirc/__init__.py
+++ pymirc/__init__.py
@@ -2,3 +2,10 @@ from . import fileio
 from . import image_operations
 from . import metrics
 from . import viewer
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("pymirc")
+except PackageNotFoundError:
+    __version__ = "unknown"
