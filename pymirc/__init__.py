eefd97a3349dad85dc6b320dcd14cd9e070392fd
Georg Schramm <georg@Georgs-MacBook-Pro-5.local>
2024-11-27 23:34:07 +0100

    use setuptools-scm to get version

--- pymirc/__init__.py
+++ pymirc/__init__.py
@@ -2,10 +2,3 @@ from . import fileio
 from . import image_operations
 from . import metrics
 from . import viewer
-
-from importlib.metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version("pymirc")
-except PackageNotFoundError:
-    __version__ = "unknown"
